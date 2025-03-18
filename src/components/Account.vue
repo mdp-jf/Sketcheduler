948f14401778039b862d1f0ed856775e804b0491
Morten Dahl Pedersen <mdp@jf-data.com>
2025-03-18 08:00:34 +0100

    corrected import path for supabase

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { supabase } from '../supabase'
+import { supabase } from '../lib/supabase'
 import { onMounted, ref, toRefs } from 'vue'
 import Avatar from './Avatar.vue'
 
